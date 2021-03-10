fd7c9af138896b0845dd4abc9f7adaf8b47582df
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2021-03-10 14:26:05 -0500

    Formulario de titulo terminado

--- Fronend/src/app/pages/paneles-administracion/panel-postulante/hoja-vida/titulos-academicos/titulos-academicos.component.ts
+++ Fronend/src/app/pages/paneles-administracion/panel-postulante/hoja-vida/titulos-academicos/titulos-academicos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {TituloModel} from 'src/app/models/titulo.models';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-titulos-academicos',
@@ -7,13 +8,21 @@ import {TituloModel} from 'src/app/models/titulo.models';
 })
 export class TitulosAcademicosComponent implements OnInit {
   instanciaTituloAcademico:TituloModel;
+  listaNivelInsturccion:string[]=["Tercer Nivel","Cuarto Nivel"];
   constructor() { }
 
   ngOnInit() {
   this.instanciaTituloAcademico=new TituloModel();
+  this.instanciaTituloAcademico.detalles_adiciones="El titulo lo tub een ";
+  this.instanciaTituloAcademico.estado=1;
+  this.instanciaTituloAcademico.evidencias_url="/c/"
+  //this.instanciaTituloAcademico.nivel_instruccion=1;
+  this.instanciaTituloAcademico.numero_registro="1b-12fc";
+  this.instanciaTituloAcademico.tipo_titulo=2;
+  this.instanciaTituloAcademico.titulo_obtenido="Ing Comercial";
   }
-  onSubMitRegistroTitulo(){
-
+  onSubMitRegistroTitulo(formRegistroTitulo:NgForm){
+    console.log(formRegistroTitulo);
 
   }
 }
