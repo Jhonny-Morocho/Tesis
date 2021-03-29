0f034613df84123c217ebb00149c1beed9a420b2
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2021-03-29 00:16:58 -0500

    Configurando Correo con reloj

--- Backend/app/Console/Kernel.php
+++ Backend/app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
      * @return void
      */
     protected function schedule(Schedule $schedule)
-    {
+    {   
         $schedule->command('command:eliminarMensaje')->everyMinute();
         //$schedule->command('command:eliminarMensaje')->everyMinute();
     }
